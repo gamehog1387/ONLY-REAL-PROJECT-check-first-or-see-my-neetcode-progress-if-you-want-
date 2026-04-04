00d2bd1ea1e7609a93070f19b3d073e2657c2a0a
gamehog1387 <47195783+gamehog1387@users.noreply.github.com>
2026-04-04 17:12:55 -0500

    Change SQL query to fetch age from employees

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #import with password and database is not viewable on github 
 #show table on python. retrieve from db
-
+#show attribute from table employees
 mycursor = projectDB.cursor(buffered=True)
 
-mycursor.execute("SELECT * FROM employees")
+mycursor.execute("SELECT age FROM employees")
 
-myresult = mycursor.fetchall()
+myresult = mycursor.fetchone()
 
 for row in myresult:
     print(row)
