1805dddfda9c72f507f2a6fdabb56eefd740c27c
gamehog1387 <47195783+gamehog1387@users.noreply.github.com>
2026-04-04 17:09:03 -0500

    Change from inserting to retrieving employee records
    Replace employee table creation and insertion with retrieval of employee records from the database.

--- app.py
+++ app.py
@@ -1,24 +1,11 @@
-
-
 #import with password and database is not viewable on github 
+#show table on python. retrieve from db
 
 mycursor = projectDB.cursor(buffered=True)
 
-mycursor.execute("CREATE TABLE IF NOT EXISTS employees (employee VARCHAR (50), age INT, pay INT)")
+mycursor.execute("SELECT * FROM employees")
 
-mycursor.execute("SHOW TABLES")
-for tb in mycursor:
-    print(tb)
-#make a exectuable for info into table employees
-sqlFormula = "INSERT INTO employees (employee, age, pay) VALUES (%s, %s, %s)"
-employees = [("Sarah", 22, 15),
-            ("Michelle", 22, 15),
-            ("Tyler", 22, 15),
-            ("Harold", 22, 15),
-            ("SANDEEEEE", 22, 15),]
+myresult = mycursor.fetchall()
 
-
-mycursor.executemany(sqlFormula, employees)
-projectDB.commit()
-
-print("Record inserted successfully")
+for row in myresult:
+    print(row)
