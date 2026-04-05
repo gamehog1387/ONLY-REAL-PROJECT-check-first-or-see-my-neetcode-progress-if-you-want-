fa2ea1d3712a7cbc2ed6be4f76b0718be662f961
gamehog1387 <47195783+gamehog1387@users.noreply.github.com>
2026-04-04 20:27:29 -0500

    Add departments table and insert records
    Created a new table 'departments' and inserted multiple records into it.

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-#import with password and database is not viewable on github 
-#show table on python. retrieve from db
-#show attribute from table employees
 mycursor = projectDB.cursor(buffered=True)
 
-mycursor.execute("SELECT age FROM employees")
+mycursor.execute(""" CREATE TABLE IF NOT EXISTS departments (department VARCHAR (50), ID INT, clearance INT, pay_avg INT)""")
 
-myresult = mycursor.fetchone()
+mycursor.execute("SHOW TABLES")
 
-for row in myresult:
-    print(row)
+for tb in mycursor:
+    print(tb)
+    #INFO FOR deparments info in departments table
+sqlFormula = "INSERT INTO departments (department, ID, clearance, pay_avg) VALUES (%s, %s, %s, %s)"
+departments = [("cyber", 3104, 1, 34),
+               ("DEV OPS", 4942, 2, 53),
+               ('software', 5932, 2, 60), 
+               ("cyber", 3104, 4, 43),
+               ("front end", 4942, 2, 24),
+               ('database', 5932, 4, 60),
+               ("IT", 3104, 2, 34),
+               ("Help Desk", 4942, 1, 25),
+               ('HR', 5932, 4, 60),]
+
+mycursor.executemany(sqlFormula, departments)
+
+projectDB.commit()
