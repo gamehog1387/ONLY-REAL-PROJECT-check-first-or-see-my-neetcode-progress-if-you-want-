8b3d3013c38ca86aa9ac770752cc44a4169a0f57
gamehog1387 <47195783+gamehog1387@users.noreply.github.com>
2026-04-04 20:30:07 -0500

    Refactor department data handling in app.py
    Removed table creation and insertion logic, added data retrieval for departments.

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
+#show data for departments
 mycursor = projectDB.cursor(buffered=True)
 
-mycursor.execute(""" CREATE TABLE IF NOT EXISTS departments (department VARCHAR (50), ID INT, clearance INT, pay_avg INT)""")
+mycursor.execute("SELECT * FROM departments")
 
-mycursor.execute("SHOW TABLES")
+myresult = mycursor.fetchall()
 
-for tb in mycursor:
-    print(tb)
-    #INFO FOR deparments info in departments table
-sqlFormula = "INSERT INTO departments (department, ID, clearance, pay_avg) VALUES (%s, %s, %s, %s)"
-departments = [("cyber", 3104, 1, 34),
-               ("DEV OPS", 4942, 2, 53),
-               ('software', 5932, 2, 60), 
-               ("cyber", 3104, 4, 43),
-               ("front end", 4942, 2, 24),
-               ('database', 5932, 4, 60),
-               ("IT", 3104, 2, 34),
-               ("Help Desk", 4942, 1, 25),
-               ('HR', 5932, 4, 60),]
-
-mycursor.executemany(sqlFormula, departments)
-
-projectDB.commit()
+for row in myresult:
+    print(row)
