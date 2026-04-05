b7d9be4ce4d89725f57ca1a72d6268eb47b15262
gamehog1387 <47195783+gamehog1387@users.noreply.github.com>
2026-04-04 23:58:57 -0500

    Insert employee data from CSV into database
    Replaced department data retrieval with CSV insertion into employees table.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-#show data for departments
+
+#show
 mycursor = projectDB.cursor(buffered=True)
 
-mycursor.execute("SELECT * FROM departments")
 
-myresult = mycursor.fetchall()
+with open('employees.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
 
-for row in myresult:
-    print(row)
+    for row in csv_reader:
+        sql = "INSERT INTO employees (employee, age, pay, dept_code, skill_level) VALUES (%s, %s, %s, %s, %s)" 
+        mycursor.execute(sql, row)
+        
+
+projectDB.commit()
+
+# INSERT CSV FILE INTO employees
