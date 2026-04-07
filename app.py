652bf98383c312f10beab4bed31951a0dd27803b
gamehog1387 <47195783+gamehog1387@users.noreply.github.com>
2026-04-06 21:15:58 -0500

    Update CSV import from employees to customer list
    Replaced employee CSV import with customer list import and added retrieval of customer data.

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
+mycursor = projectDB.cursor(buffered="True")
 
-#show
-mycursor = projectDB.cursor(buffered=True)
+mycursor.execute("SELECT * FROM customer_list")
 
-
-with open('employees.csv', mode='r') as file:
+myresult = mycursor.fetchall()
+with open('customer_list.csv', mode='r') as file:
     csv_reader = csv.reader(file)
     next(csv_reader)
 
     for row in csv_reader:
-        sql = "INSERT INTO employees (employee, age, pay, dept_code, skill_level) VALUES (%s, %s, %s, %s, %s)" 
-        mycursor.execute(sql, row)
-        
+        sql = "INSERT INTO customer_list (customer, contracts_done, VALUE, MONEY_MADE, complaints) VALUES (%s, %s, %s, %s, %s)"
+        mycursor.execute(sql, row )
 
+    for row in myresult:
+        print(row)
 projectDB.commit()
-
-# INSERT CSV FILE INTO employees
