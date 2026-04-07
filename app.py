4a14197d91d0fdf67184b180eb636d8b61dbb280
gamehog1387 <47195783+gamehog1387@users.noreply.github.com>
2026-04-06 23:35:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-mycursor = projectDB.cursor(buffered=True)
+#updat every value in my sql column dept_code in employees
 
-mycursor.execute("SELECT * FROM projects")
-
-myresult = mycursor.fetchall()
-with open('projects.csv', mode='r') as file:
-    csv_reader = csv.reader(file)
-    next(csv_reader)
-
-    for row in csv_reader:
-        sql = "INSERT INTO projects (project, dept_helping, successful_proj, percent_done, completed_projects, customer_names ) VALUES (%s, %s, %s, %s, %s, %s)"
-        mycursor.execute(sql, row )
-
-    for row in myresult:
-        print(row)
-projectDB.commit()
+UPDATE employees
+SET dept_code = FLOOR(1 + RAND() * 10);
